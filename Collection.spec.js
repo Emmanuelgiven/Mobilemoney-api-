02e0f0416f2d8ddd2ff27f8c99d4b0c00a4f3cf6
Emmanuelgiven <60091432+Emmanuelgiven@users.noreply.github.com>
2023-02-23 12:36:52 +0300

    Collection.spec.js

---
+++ Collection.spec.js
@@ -0,0 +1,73 @@
+
+// Import the module
+const { useUserProvisioning, useCollections } = require('mtn-momo');
+
+const subscriptionKey = 'COLLECTIONS_PRIMARY_KEY';
+
+// (sandbox/development environment only) Provision/create a user and api key
+const sandboxUserInfo = await useUserProvisioning.createApiUserAndKey({
+  subscriptionKey: subscriptionKey,
+  providerCallbackHost: 'PROVIDER_CALLBACK_HOST'
+});
+const { userId, apiKey, targetEnvironment } = sandboxUserInfo;
+
+// Initialize the wrapper
+const collections = useCollections({
+  subscriptionKey,
+  apiKey,
+  userId,
+  targetEnvironment
+});
+
+/* Collections API */
+
+// (optional) Get an access token
+const token = await collections.getToken();
+const { token_type, access_token, expires_in } = token;
+
+// Check if an account is active. Returns a boolean value
+const isActive = await collections.isAccountActive({
+  accountHolderIdType: 'msisdn',
+  accountHolderId: 'PHONE_NUMBER'
+});
+
+// Submit a request for payment
+const paymentOptions = {
+  amount: 15000,
+  currency: 'EUR',
+  externalId: '123456789',
+  payer: {
+    partyIdType: 'msisdn',
+    partyId: 'PHONE_NUMBER'
+  },
+  payerMessage: 'message',
+  payeeNote: 'note'
+};
+const transactionId = await collections.useiate({
+  callbackUrl: 'http://test1.com',
+  paymentOptions: paymentOptions
+});
+
+// Check the status of a request for payment
+const transaction = await collections.fetchTransaction(transactionId);
+const {
+  amount,
+  currency,
+  financialTransactionId,
+  externalId,
+  payer: {
+    partyIdType,
+    partyId
+  },
+  status: 'SUCCESSFUL|FAILED|PENDING',
+  reason: {
+    code,
+    message
+  }
+} = transaction;
+
+// Check my account balance
+const accountBalance = await collections.fetchAccountBalance();
+const { currency, availableBalance } = accountBalance;
+
+/* End Collections API */
